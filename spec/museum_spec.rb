bf08008646cff7e727c6f17b7459efc78f54bb5f
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-01-03 10:55:18 -0700

    Museum #patron_by_exhibit_interest spec created

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe Museum do
   let(:imax) { Exhibit.new({name: "IMAX", cost: 15}) }
   let(:patron_1) { Patron.new("Bob", 20) }
   let(:patron_2) { Patron.new("Sally", 20) }
-
+  let(:patron_3) { Patron.new("Johnny", 5) }
+  
   describe '#initialize' do
     it 'exists' do
       expect(dmns).to be_a(Museum)
@@ -45,4 +46,25 @@ RSpec.describe Museum do
     end
   end
   
+  describe '#patron_by_exhibit_interest' do
+    it 'can have patrons' do
+      patron_1 = Patron.new("Bob", 0)
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      expect(patrons).to eq([])
+
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3.add_interest("Dead Sea Scrolls")
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+    end
+  end
 end
