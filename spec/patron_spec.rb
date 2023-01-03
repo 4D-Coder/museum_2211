a41e75c95a05361ac6bd968aa9eafea3a305889b
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-01-03 09:20:21 -0700

    Patron #initialize spec created

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,3 +1,18 @@
 require 'rspec'
 require './lib/patron'
 
+RSpec.describe Patron do
+  let(:patron_1) { Patron.new("Bob", 20) }
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(patron_1).to be_a(Patron)
+    end
+
+    it 'has attributes' do
+      expect(patron_1.name).to eq('Bob')
+      expect(patron_1.spending_money).to eq(20)
+      expect(patron_1.interests).to eq([])
+    end
+  end
+end
